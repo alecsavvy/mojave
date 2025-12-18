ad20fd2fb78e5bcb2affd6f7ffc3b3e0cf770c37
Alec Savvy <alecjsavvy@gmail.com>
2025-12-18 09:00:27 -0800

    website stubs

--- site/vocs.config.ts
+++ site/vocs.config.ts
@@ -18,6 +18,51 @@ export default defineConfig({
       link: '/onepager',
     },
     {
+      text: 'Primitives',
+      items: [
+        { text: 'DDEX', link: '/primitives/ddex' },
+        { text: 'CometBFT', link: '/primitives/cometbft' },
+        { text: 'OpenTDF', link: '/primitives/opentdf' },
+        { text: 'BitTorrent', link: '/primitives/bittorrent' },
+        { text: 'ConnectRPC', link: '/primitives/connectrpc' },
+      ],
+    },
+    {
+      text: 'Architecture',
+      items: [
+        { text: 'Overview', link: '/architecture/overview' },
+        { text: 'Metadata', link: '/architecture/metadata' },
+        { text: 'Upload', link: '/architecture/upload' },
+        { text: 'File Replication', link: '/architecture/file-replication' },
+      ],
+    },
+    {
+      text: 'Economics',
+      items: [
+        { text: 'VOX', link: '/economics/vox' },
+        { text: 'Purchases', link: '/economics/purchases' },
+      ]
+    },
+    {
+      text: 'Actors',
+      items: [
+        { text: 'Operators', items: [
+          { text: 'Validators', link: '/actors/validators' },
+          { text: 'Archivers', link: '/actors/archivers' },
+          { text: 'Oracles', link: '/actors/oracles' },
+        ]},
+        { text: 'Producers', items: [
+          { text: 'Distributors', link: '/actors/distributors' },
+          { text: 'Labels', link: '/actors/labels' },
+          { text: 'Artists', link: '/actors/artists' },
+        ]},
+        { text: 'Consumers', items: [
+          { text: 'DSPs', link: '/actors/dsps' },
+          { text: 'Listeners', link: '/actors/listeners' },
+        ]},
+      ],
+    },
+    {
       text: 'Glossary',
       link: '/glossary',
     },
