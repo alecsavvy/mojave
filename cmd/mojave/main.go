5d51a0fce69786278f6bafa582d830d2600d0d69
Alec Savvy <alecjsavvy@gmail.com>
2026-02-21 07:15:27 -0700

    reset

--- cmd/mojave/main.go
+++ cmd/mojave/main.go
@@ -1,21 +1,7 @@
 package main
 
-import (
-	"context"
-	"fmt"
-	"os"
-	"os/signal"
-	"syscall"
-
-	"github.com/alecsavvy/mojave/commands"
-)
+import "fmt"
 
 func main() {
-	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
-	defer stop()
-
-	if err := commands.Execute(ctx); err != nil {
-		fmt.Printf("boom: %v\n", err)
-		os.Exit(1)
-	}
+	fmt.Println("Hello, World!")
 }
--- site/README.md
+++
@@ -1,3 +0,0 @@
-# Mojave Website
-
-This is the Mojave website built with [Vocs](https://vocs.dev). Found at [mojave.audio](https://mojave.audio).
